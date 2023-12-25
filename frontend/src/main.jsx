ca25f3e75415c6ef0577f8b1031257a9e1716ecc
auxload <gd69435@gmail.com>
2023-12-25 15:19:36 +0200

    more

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
